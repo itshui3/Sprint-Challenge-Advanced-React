20955ecbc5c95495dfc6008ddb55e7a63f399470
Hui <jeffrey.x.hui@gmail.com>
2019-12-06 08:47:09 -0800

    wrote useFavoritism hook

---
+++ client/src/hooks/useFavoritism.js
@@ -0,0 +1,18 @@
+import { useLocalStorage } from './useLocalStorage'
+
+export const useFavoritism = initialFavorites => {
+  const [favorites, setFavorites] = useLocalStorage('favorites', initialFavorites)
+
+  const setNewFavorite = player => {
+    if (favorites.every(person => {
+      return person.name !== player.name
+    })) {
+      setFavorites([...favorites, player])
+    }
+  }
+  const removeFavorite = favoriteIndex => {
+    setFavorites(favorites.slice(0, favoriteIndex).concat(favorites.slice(index + 1)))
+  }
+
+  return [favorites, setNewFavorite, removeFavorite]
+}

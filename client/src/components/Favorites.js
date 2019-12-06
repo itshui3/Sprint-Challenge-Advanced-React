7eda95a8a3551fa09c10bba4e9d2754d392ac48f
Hui <jeffrey.x.hui@gmail.com>
2019-12-06 10:14:53 -0800

    set up modal with edit for full CRUD

--- client/src/components/Favorites.js
+++ client/src/components/Favorites.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useModal } from '../hooks/useModal'
 
 export default function Favorites(props) {
 
+  const [playerOfInterest, setPlayerOfInterest] = useState(-1)
+  const [description, setDescription] = useState('')
+  const [modal, setModal] = useState(false)
+
+  let modalDisplay = modal ? 'display-block modal' : 'display-none modal'
+
+  const toggleModal = ev => {
+    setModal(!modal)
+  }
+  const loadModal = ev => {
+    setPlayerOfInterest(ev.target.id)
+    setModal(!modal)
+  }
+  const handleDescription = ev => {
+    setDescription(ev.target.value)
+  }
+  const handleSubmit = ev => {
+    ev.preventDefault()
+    props.describeFavorite(description, playerOfInterest)
+  }
+
   return (
     <div>
+
+      <div className={modalDisplay}>
+        <div className="modal-main">
+          <form onSubmit={handleSubmit}>
+            <input 
+              placeholder="Description"  
+              onChange={handleDescription}
+              value={description}
+            />
+            <button>Edit</button>
+          </form>
+          {
+            playerOfInterest.description && 
+            <p>{props.favorites[playerOfInterest].description}</p>
+          }
+          <button onClick={toggleModal}>Close Modal</button>
+        </div>
+      </div>
+
       {
         props.favorites && props.favorites.map((favorite, index) => (
           <div key={index}>
@@ -13,11 +53,12 @@ export default function Favorites(props) {
             {
               favorite.description && <p>{favorite.description}</p>
             }
-
+            <button id={index} onClick={loadModal}>Edit Description</button>
             <button onClick={() => props.removeFavorite(index)}>Remove Favorite</button>
           </div>
         ))
       }
+
     </div>
   )
 }
